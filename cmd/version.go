79dd4fc3428b8d4e473cf10105d3550da68a7de7
gdanko <gdanko@protonmail.com>
2025-10-12 14:10:05 -0700

    Version

---
+++ cmd/version.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/gdanko/rdbak/pkg/raindrop"
+	"github.com/spf13/cobra"
+)
+
+var (
+	versionCmd = &cobra.Command{
+		Use:          "version",
+		Short:        "Print the current rdbak version",
+		Long:         "Print the current rdbak version",
+		RunE:         runVersionCmd,
+		SilenceUsage: true,
+	}
+)
+
+func init() {
+	versionCmd.Flags().BoolVarP(&versionFull, "full", "f", false, "Display more version information")
+	rootCmd.AddCommand(versionCmd)
+}
+
+func runVersionCmd(cmd *cobra.Command, args []string) error {
+	fmt.Fprintf(cmd.OutOrStdout(), "%s\n", raindrop.Version(true, true, versionFull))
+
+	return nil
+}
