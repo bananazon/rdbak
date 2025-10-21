0c8b31cf45a1e183aaedbc2c2dead778832cbfa4
gdanko <gdanko@protonmail.com>
2025-10-21 08:25:45 -0700

    Use a table creator with pagination

--- cmd/03_list_raindrops.go
+++ cmd/03_list_raindrops.go
@@ -1,52 +1,70 @@
 package cmd
 
 import (
-	"os"
+	"fmt"
 	"strconv"
 	"strings"
 
-	"github.com/olekukonko/tablewriter"
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
 )
 
 var (
-	listCmd = &cobra.Command{
-		Use:          "list",
-		Aliases:      []string{"l", "ls"},
-		Short:        "List the bookmarks in your raindrop.io account",
-		Long:         "List the bookmarks in your raindrop.io account",
-		PreRun:       listPreRunCmd,
-		Run:          listRunCmd,
+	listRaindropsCmd = &cobra.Command{
+		Use:          "list-raindrops",
+		Aliases:      []string{"lr"},
+		Short:        "List the raindrops in your raindrop.io account",
+		Long:         "List the raindrops in your raindrop.io account",
+		PreRun:       listRaindropsPreRunCmd,
+		Run:          listRaindropsRunCmd,
 		SilenceUsage: false,
 	}
 )
 
 func init() {
-	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(listRaindropsCmd)
 }
 
-func listPreRunCmd(cmd *cobra.Command, args []string) {
+func listRaindropsPreRunCmd(cmd *cobra.Command, args []string) {
 
 }
 
-func listRunCmd(cmd *cobra.Command, args []string) {
-	raindrops, err := rd.List()
+func listRaindropsRunCmd(cmd *cobra.Command, args []string) {
+	raindrops, err := rd.ListRaindrops()
 	if err != nil {
 		logger.Error(err)
 		logger.Exit(1)
 	}
 
-	tableOut := [][]string{}
-	for _, raindrop := range raindrops {
-		tableOut = append(tableOut, []string{
+	collections, err := rd.ListCollections()
+	if err != nil {
+		logger.Error(err)
+		logger.Exit(1)
+	}
+
+	t := table.NewWriter()
+	t.SetStyle(table.StyleLight)
+	// t.SetColumnConfigs([]table.ColumnConfig{{Name: "Link", WidthMax: 80}})
+	t.SetPageSize(40)
+	t.AppendHeader(table.Row{"ID", "Collection", "Link", "Tags"})
+
+	for idx, raindrop := range raindrops {
+		collectionId := raindrops[idx].Collection.Id
+		collection, exists := collections[uint64(collectionId)]
+		if exists {
+			if raindrops[idx].Collection.Id > 0 {
+				raindrops[idx].Collection.Name = collection.Title
+			} else {
+				raindrops[idx].Collection.Name = "N/A"
+			}
+		}
+		t.AppendRow(table.Row{
 			strconv.Itoa(int(raindrop.Id)),
-			raindrop.Type,
+			raindrop.Collection.Name,
 			raindrop.Link,
 			strings.Join(raindrop.Tags, ","),
 		})
 	}
-	table := tablewriter.NewWriter(os.Stdout)
-	table.Header([]string{"ID", "Type", "Link", "Tags"})
-	table.Bulk(tableOut)
-	table.Render()
+
+	fmt.Println(t.Render())
 }
