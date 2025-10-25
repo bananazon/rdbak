7093a77229bfbd0a91b65df070475d082910f03b
gdanko <gdanko@protonmail.com>
2025-10-25 08:27:29 -0700

    Use err.Error()

--- cmd/collections/sort.go
+++ cmd/collections/sort.go
@@ -2,6 +2,7 @@ package collections
 
 import (
 	"github.com/bananazon/raindrop/pkg/context"
+	"github.com/bananazon/raindrop/pkg/data"
 	"github.com/bananazon/raindrop/pkg/raindrop"
 	"github.com/spf13/cobra"
 )
@@ -14,16 +15,18 @@ func newSortCollectionsCmd(ctx *context.AppContext) (c *cobra.Command) {
 		PreRunE: func(cmdC *cobra.Command, args []string) error {
 			rd, err := raindrop.New(ctx.RaindropHome, ctx.RaindropConfig, ctx.Logger)
 			if err != nil {
-				ctx.Logger.Println("Failed to initialize raindrop:", err)
+				ctx.Logger.Println("Failed to initialize raindrop:", err.Error())
 				return err
 			}
 			ctx.RD = rd
 			return nil
 		},
 		RunE: func(cmdC *cobra.Command, args []string) error {
-			_, err := ctx.RD.API.SortCollections(ctx.FlagCollectionsSortOrder)
+			_, err := ctx.RD.API.SortCollections(data.SortCollectionPayload{
+				Sort: ctx.FlagCollectionsSortOrder,
+			})
 			if err != nil {
-				ctx.Logger.Println("Failed to sort the collection:", err)
+				ctx.Logger.Println("Failed to sort the collection:", err.Error())
 				return err
 			}
 			ctx.Logger.Println("Successfully sorted the collection")
