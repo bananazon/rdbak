08b88a69b4b0149ccf855c9ee0d42fdba9dcd27a
gdanko <gdanko@protonmail.com>
2025-10-25 08:29:53 -0700

    Use payload struct

--- cmd/collections/add.go
+++ cmd/collections/add.go
@@ -2,6 +2,7 @@ package collections
 
 import (
 	"github.com/bananazon/raindrop/pkg/context"
+	"github.com/bananazon/raindrop/pkg/data"
 	"github.com/bananazon/raindrop/pkg/raindrop"
 	"github.com/spf13/cobra"
 )
@@ -14,20 +15,20 @@ func newAddCollectionCmd(ctx *context.AppContext) (c *cobra.Command) {
 		PreRunE: func(cmdC *cobra.Command, args []string) error {
 			rd, err := raindrop.New(ctx.RaindropHome, ctx.RaindropConfig, ctx.Logger)
 			if err != nil {
-				ctx.Logger.Println("Failed to initialize raindrop:", err)
+				ctx.Logger.Println("Failed to initialize raindrop:", err.Error())
 				return err
 			}
 			ctx.RD = rd
 			return nil
 		},
 		RunE: func(cmdC *cobra.Command, args []string) error {
-			_, err := ctx.RD.API.AddCollection(
-				ctx.FlagAddCollectionTitle,
-				ctx.FlagAddCollectionParent,
-				ctx.FlagAddCollectionPublic,
-			)
+			_, err := ctx.RD.API.AddCollection(data.AddCollectionPayload{
+				Title:  ctx.FlagAddCollectionTitle,
+				Parent: ctx.FlagAddCollectionParent,
+				Public: ctx.FlagAddCollectionPublic,
+			})
 			if err != nil {
-				ctx.Logger.Println("Failed to add the collection:", err)
+				ctx.Logger.Println("Failed to add the collection:", err.Error())
 				return err
 			}
 			ctx.Logger.Println("Successfully added the collection")
