601e0e636b137235feba127ed099d33484fb7288
gdanko <gdanko@protonmail.com>
2025-10-25 16:05:48 -0700

    Use token

--- pkg/raindrop/new.go
+++ pkg/raindrop/new.go
@@ -19,9 +19,8 @@ const Day = 24 * time.Hour
 const Week = 7 * Day
 
 type Config struct {
-	Email             string `yaml:"email"`
-	Password          string `yaml:"password,omitempty"`
-	EncryptedPassword string `yaml:"encryptedPassword,omitempty"`
+	EncryptedToken string `yaml:"encryptedApiToken,omitempty"`
+	Token          string `yaml:"apiToken,omitempty"`
 }
 
 type Raindrop struct {
@@ -32,9 +31,10 @@ type Raindrop struct {
 	CollectionsFile    string
 	Config             *Config
 	ConfigPath         string
-	RaindropRoot       string
 	Logger             *logrus.Logger
 	PruneOlder         bool
+	RaindropRoot       string
+	Token              string
 	UpdatedBookmarks   []*data.Bookmark
 	UpdatedCollections []*data.Collection
 }
@@ -48,6 +48,7 @@ func New(raindropRoot string, configPath string, logger *logrus.Logger) (r *Rain
 		PruneOlder:   false,
 		RaindropRoot: raindropRoot,
 	}
+
 	r.Bookmarks = make(map[uint64]*data.Bookmark)
 	r.Collections = make(map[uint64]*data.Collection)
 
@@ -59,10 +60,7 @@ func New(raindropRoot string, configPath string, logger *logrus.Logger) (r *Rain
 	r.BookmarksFile = filepath.Join(raindropRoot, "bookmarks.yaml")
 	r.CollectionsFile = filepath.Join(raindropRoot, "collections.yaml")
 
-	err = r.API.Login(r.Config.Email, r.Config.Password)
-	if err != nil {
-		return r, err
-	}
+	r.API.Token = r.Config.Token
 
 	return r, nil
 }
@@ -77,14 +75,14 @@ func (r *Raindrop) ParseConfig() (err error) {
 		return fmt.Errorf("failed to parse %s", r.ConfigPath)
 	}
 
-	if len(r.Config.Password) == 0 {
-		if len(r.Config.EncryptedPassword) > 0 {
-			err = r.DecryptPassword()
+	if len(r.Config.Token) == 0 {
+		if len(r.Config.EncryptedToken) > 0 {
+			err = r.DecryptToken()
 			if err != nil {
 				return err
 			}
 		} else {
-			return fmt.Errorf("both password and passwordEncrypted fields in the config are empty; please fix this")
+			return fmt.Errorf("both apiToken and apiTokenEncrypted fields in the config are empty; please fix this")
 		}
 	}
 
