7535d56515f80ad40ff7d93507f2e3412560ba27
gdanko <gdanko@protonmail.com>
2025-10-24 20:25:21 -0700

    Add a command to back up collections

---
+++ cmd/collections/backup.go
@@ -0,0 +1,37 @@
+package collections
+
+import (
+	"github.com/bananazon/raindrop/pkg/context"
+	"github.com/bananazon/raindrop/pkg/raindrop"
+	"github.com/spf13/cobra"
+)
+
+func newBackupCollectionsCmd(ctx *context.AppContext) (c *cobra.Command) {
+	c = &cobra.Command{
+		Use:     "backup",
+		Aliases: []string{"b"},
+		Short:   "Back your raindrop.io collections up to a YAML file",
+		PreRunE: func(cmdC *cobra.Command, args []string) error {
+			rd, err := raindrop.New(ctx.RaindropHome, ctx.RaindropConfig, ctx.Logger)
+			if err != nil {
+				ctx.Logger.Println("Failed to initialize raindrop:", err)
+				return err
+			}
+			ctx.RD = rd
+			return nil
+		},
+		RunE: func(cmdC *cobra.Command, args []string) error {
+			err := ctx.RD.BackupCollections(ctx.FlagPrune)
+			if err != nil {
+				ctx.Logger.Println("Backup failed:", err)
+				return err
+			}
+			ctx.Logger.Println("Backup successful")
+			return nil
+		},
+	}
+
+	ctx.GetBackupFlags(c)
+
+	return c
+}
